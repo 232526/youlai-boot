6e5351d7f036ff33fc1fe8b2b90444426658e048
tom <tom@zen-game.cn>
2026-04-08 16:27:39 +0800

    查询修改

--- src/main/java/com/youlai/boot/market/order/model/query/SmsPhoneRecordQuery.java
+++ src/main/java/com/youlai/boot/market/order/model/query/SmsPhoneRecordQuery.java
@@ -19,11 +19,20 @@ public class SmsPhoneRecordQuery extends BaseQuery {
     @Schema(description = "订单编号")
     private String orderNo;
 
+    @Schema(description = "国家ID")
+    private Long countryId;
+
     @Schema(description = "手机号")
     private String phoneNumber;
 
     @Schema(description = "发送状态：0=待发送，1=发送成功，2=发送失败")
     private Integer sendStatus;
+
+    @Schema(description = "开始日期")
+    private String startDate;
+
+    @Schema(description = "结束日期")
+    private String endDate;
 
     @Schema(description = "创建时间")
     private String createTime;
